d3c544d36eeadfba60e45ff0e13a3688625f50b0
Rishikesh Sharma(Rk_botz) <118276768+Rishikesh-Sharma09@users.noreply.github.com>
2024-05-10 17:12:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -15,23 +15,28 @@ from utils import get_settings, get_size, is_subscribed, is_check_admin, get_sho
 import requests
 from telegraph import upload_file
 
-@Client.on_message(filters.command("ask") & filters.group(SUPPORT_GROUP) & filters.incoming) #add your support grp
+@Client.on_message(filters.command("ask") & filters.incoming) #add your support grp
 async def aiRes(_, message):
-    asked = message.text.split(None, 1)[1]
-    if not asked:
-        return await message.reply("Bhai kuch puch to le /ask k baad !")
-    thinkStc = await message.reply_sticker(sticker=random.choice(STICKERS_IDS))
-    url = f"https://bisal-ai-api.vercel.app/biisal" 
-    res = requests.post(url , data={'query' : asked})
-    if res.status_code == 200:
-        response = res.json().get("response")
-        await thinkStc.delete()
-        await message.reply(f"<b>hey {message.from_user.mention()},\n{response.lstrip() if response.startswith(' ') else response}</b>")
+    if message.chat.id == SUPPORT_GROUP:
+        asked = message.text.split(None, 1)[1]
+        if not asked:
+            return await message.reply("Bhai kuch puch to le /ask k baad !")
+        thinkStc = await message.reply_sticker(sticker=random.choice(STICKERS_IDS))
+        url = f"https://bisal-ai-api.vercel.app/biisal" 
+        res = requests.post(url , data={'query' : asked})
+        if res.status_code == 200:
+            response = res.json().get("response")
+            await thinkStc.delete()
+            await message.reply(f"<b>hey {message.from_user.mention()},\n{response.lstrip() if response.startswith(' ') else response}</b>")
+        else:
+            await thinkStc.delete()
+            await message.reply("Mausam kharab hai ! Thode der mein try kre !\nor Report to Developer.")
     else:
-        await thinkStc.delete()
-
-        await message.reply("Mausam kharab hai ! Thode der mein try kre !\nor Report to Developer.")
-
+        btn = [[
+            InlineKeyboardButton('💡 Support Group 💡', url=SUPPORT_LINK)
+        ]]
+        await message.reply(f"<b>hey {message.from_user.mention},\n\nPlease use this command in support group.</b>", reply_markup=InlineKeyboardMarkup(btn))
+        
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     botid = client.me.id
