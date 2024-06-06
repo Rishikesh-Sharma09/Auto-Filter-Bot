dad941f35a62d0aeed6e9aa7e91c873266a08389
BeingAnonymous4u <155914718+BeingAnonymous4u@users.noreply.github.com>
2024-06-07 01:29:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -100,8 +100,8 @@ STICKERS_IDS = (
 
 # boolean settings 
 GROUP_FSUB = is_enabled('GROUP_FSUB', False) 
-PM_SEARCH = is_enabled('PM_SEARCH', False) #switch True or False for searching results in bot pm😃
-IS_VERIFY = is_enabled('IS_VERIFY', True)
+PM_SEARCH = is_enabled('PM_SEARCH', True) #switch True or False for searching results in bot pm😃
+IS_VERIFY = is_enabled('IS_VERIFY', False)
 AUTO_DELETE = is_enabled('AUTO_DELETE', False)
 WELCOME = is_enabled('WELCOME', False)
 PROTECT_CONTENT = is_enabled('PROTECT_CONTENT', False)
